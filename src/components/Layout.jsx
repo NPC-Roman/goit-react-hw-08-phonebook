6f703a010074522792d4fb12b36f73269070eee3
NPC-Roman <128088564+NPC-Roman@users.noreply.github.com>
2023-11-19 23:50:47 +0200

    change12

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from 'react-router-dom';
-// import { Toaster } from 'react-hot-toast';
 import { AppBar } from './AppBar/AppBar';
 import { Suspense } from 'react';
 
--- src/index.js
+++ src/index.js
@@ -1,26 +1,9 @@
-/*
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-import App from 'components/App';
-
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
-*/
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
+
 import './index.css';
+
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
